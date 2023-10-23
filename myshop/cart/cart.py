d0671d6c971b17c3c73fc4ca03451f866b766a60
desirelesson <theproblemdi@gmail.com>
2023-10-23 23:04:38 +0300

    implemented add,init,save,remove in cart.py

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -11,4 +11,32 @@ class Cart:
             # сохранить пустую корзину в сеансе
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+
+    def add(self, product, quantity=1, override_quantity=False):
+        """Добавить товар в корзину либо обновить его количество."""
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
+        if override_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity 
+        self.save()
+
+
+    def save(self):
+        # пометить сеанс как "измененный",
+        # чтобы обеспечить его сохранение
+        self.session.modified = True
+
+    
+    def remove(self, product):
+        """Удалить товар из корзины."""
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
         
