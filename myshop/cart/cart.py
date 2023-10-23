c8273a2358676db0998424b83e2020087218bae4
desirelesson <theproblemdi@gmail.com>
2023-10-23 23:33:26 +0300

    implemented iter, len, get_total_price and clear method in class Cart

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -12,6 +12,27 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
+    
+    def __iter__(self):
+        """Прокрутить товарные позиции корзины в цикле и получить товары из базы данных."""
+        products_ids = self.cart.keys()
+        #получить обьекты product  и добавить из в корзину
+        products = Product.objects.filter(id__in=products_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product 
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item 
+
+        
+        def __len__(self):
+            """Подсчитать все товарные позиции в корзине."""
+            return sum(item['quantity'] for item in self.cart.values())
+
+
+
 
     def add(self, product, quantity=1, override_quantity=False):
         """Добавить товар в корзину либо обновить его количество."""
@@ -39,4 +60,14 @@ class Cart:
             del self.cart[product_id]
             self.save()
 
+
+    def get_total_price(self):
+        return sum(Decimal(item['price'] * item['quantity']
+                           for item in self.cart.values()))
+    
+
+    def clear(self):
+        # удалить корзину из сеанса
+        del self.session[settings.CART_SESSION_ID] 
+        self.save()
         
