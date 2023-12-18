d24ce95f587e9da84bd959030fde255d1ac96ad0
Desirelesson <theproblemdi@gmail.com>
2023-12-18 09:23:55 +0300

    create recommended.py for recomendation goods

--- myshop/shop/recommender.py
+++ myshop/shop/recommender.py
@@ -17,5 +17,32 @@ class Recommender:
             # получить другие товары, купленные
             # вместе с каждым товаром
             if product_id != with_id:
-                # увеличить балл товара,купленного вместе
+                # увеличить балл товара, который принадлежит одному заказу
                 r.zincrby(self.get_product_key(product_id), 1, with_id)
+
+    def suggest_product_for(self, products, max_result=6):
+        product_ids = [p.id for p in products] 
+        if len(products) == 1:
+            suggestions = r.zrange(self.get_product_key(product_ids[0]), 0, -1, desc=True)[:max_result] 
+        else:
+            # сгенерировать временный ключ
+            flat_ids = ''.join([str(id) for id in product_ids])
+            tmp_key = f'tmp_{flat_ids}'
+            # несколько товаров, объединить баллы всех товаров
+            # сохранить полученное сортированное множество
+            # во временном ключе
+            keys = [self.get_product_key(id) for id in product_ids] 
+            # удалить идентификаторы товаров,
+            # для которых дается рекомендация
+            r.zrem(tmp_key, *product_ids) 
+            # получить идентификаторы товаров по их количеству,
+            #сортировка по убыванию
+            suggestions = r.zrange(tmp_key, 0, -1, desc=True)[:max_result] 
+            r.delete(tmp_key)
+        suggested_product_ids = [int(id) for id in suggestions]
+        # получить предлагаемые товары и
+        # отсортировать их по порядку их появления
+        suggested_products = list(Product.objects.filter(
+            id__in=suggested_product_ids)) 
+        suggested_products.sort(key=lambda x: suggested_product_ids.index(x.id))
+        return suggested_products
