575e3c57b4968b9ad0c7cac89cbc58b3320e6268
Desirelesson <theproblemdi@gmail.com>
2023-12-18 09:48:31 +0300

    add recommended products in views.py

--- myshop/shop/recommender.py
+++ myshop/shop/recommender.py
@@ -1,53 +1,46 @@
 import redis
 from django.conf import settings
-from .models import Product 
+from .models import Product
 
 
-#соединить с редис 
-r = redis.Redis(host=settings.REDIS_HOST, port=set.REDIS_PORT, db=settings.REDIS_DB) 
+# connect to redis
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
+
 
 class Recommender:
     def get_product_key(self, id):
         return f'product:{id}:purchased_with'
 
-
-    def product_bought(self, products):
+    def products_bought(self, products):
         product_ids = [p.id for p in products]
         for product_id in product_ids:
-            # получить другие товары, купленные
-            # вместе с каждым товаром
-            if product_id != with_id:
-                # увеличить балл товара, который принадлежит одному заказу
-                r.zincrby(self.get_product_key(product_id), 1, with_id)
+            for with_id in product_ids:
+                if product_id != with_id:
+                    r.zincrby(self.get_product_key(product_id),
+                              1,
+                              with_id)
 
-    def suggest_product_for(self, products, max_result=6):
-        product_ids = [p.id for p in products] 
+    def suggest_products_for(self, products, max_results=6):
+        product_ids = [p.id for p in products]
         if len(products) == 1:
-            suggestions = r.zrange(self.get_product_key(product_ids[0]), 0, -1, desc=True)[:max_result] 
+            suggestions = r.zrange(
+                             self.get_product_key(product_ids[0]),
+                             0, -1, desc=True)[:max_results]
         else:
-            # сгенерировать временный ключ
             flat_ids = ''.join([str(id) for id in product_ids])
             tmp_key = f'tmp_{flat_ids}'
-            # несколько товаров, объединить баллы всех товаров
-            # сохранить полученное сортированное множество
-            # во временном ключе
-            keys = [self.get_product_key(id) for id in product_ids] 
-            # удалить идентификаторы товаров,
-            # для которых дается рекомендация
-            r.zrem(tmp_key, *product_ids) 
-            # получить идентификаторы товаров по их количеству,
-            #сортировка по убыванию
-            suggestions = r.zrange(tmp_key, 0, -1, desc=True)[:max_result] 
+            keys = [self.get_product_key(id) for id in product_ids]
+            r.zunionstore(tmp_key, keys)
+            r.zrem(tmp_key, *product_ids)
+            suggestions = r.zrange(tmp_key, 0, -1,
+                                   desc=True)[:max_results]
             r.delete(tmp_key)
-        suggested_product_ids = [int(id) for id in suggestions]
-        # получить предлагаемые товары и
-        # отсортировать их по порядку их появления
-        suggested_products = list(Product.objects.filter(
-            id__in=suggested_product_ids)) 
-        suggested_products.sort(key=lambda x: suggested_product_ids.index(x.id))
+        suggested_products_ids = [int(id) for id in suggestions]
+        suggested_products = list(Product.objects.filter(id__in=suggested_products_ids))
+        suggested_products.sort(key=lambda x: suggested_products_ids.index(x.id))
         return suggested_products
-
-    
 
     def clear_purchases(self):
         for id in Product.objects.values_list('id', flat=True):
