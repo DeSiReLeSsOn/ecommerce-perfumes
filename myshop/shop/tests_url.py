5cf35054657b62ecdeb549ab9b7def82edc186a7
desirelesson <theproblemdi@gmail.com>
2023-12-24 16:29:20 +0300

    add test_urls in shop app

--- myshop/shop/tests_url.py
+++ myshop/shop/tests_url.py
@@ -7,12 +7,18 @@ class TestUrls(SimpleTestCase):
 
 
     def test_list_url(self):
+        """Тест на получение URL-адреса для списка продуктов"""
         url = reverse('shop:product_list')
         self.assertEquals(resolve(url).func, product_list)
 
 
     def test_list_by_category_url(self):
+        """Тест на получение URL-адреса для списка продуктов по категориям"""
         url = reverse('shop:product_list_by_category', args=['any-slug'])
         self.assertEquals(resolve(url).func, product_list)
 
 
+    def test_detail_url(self):
+        """Тест на получение URL-адреса деталей продукта"""
+        url = reverse('shop:product_detail', kwargs={'id': 1, 'slug': 'any_slug'})
+        self.assertEquals(resolve(url).func, product_detail)
