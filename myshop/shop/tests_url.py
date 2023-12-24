eda98a9fd0b0e6dcf0616557aa75a494b43ec291
desirelesson <theproblemdi@gmail.com>
2023-12-24 16:15:39 +0300

    testing list_by_category

--- myshop/shop/tests_url.py
+++ myshop/shop/tests_url.py
@@ -9,3 +9,10 @@ class TestUrls(SimpleTestCase):
     def test_list_url(self):
         url = reverse('shop:product_list')
         self.assertEquals(resolve(url).func, product_list)
+
+
+    def test_list_by_category_url(self):
+        url = reverse('shop:product_list_by_category', args=['any-slug'])
+        self.assertEquals(resolve(url).func, product_list)
+
+
