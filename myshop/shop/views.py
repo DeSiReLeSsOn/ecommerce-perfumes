a10291acf24e51b52802895255cd65c8b6b9a0ce
desirelesson <theproblemdi@gmail.com>
2024-01-02 22:47:15 +0300

    fix paginator bag

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -4,7 +4,7 @@ from cart.forms import CartAddProductForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 
-def product_list(request, category_slug=None):
+"""def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     product = Product.objects.filter(available=True)
@@ -23,7 +23,22 @@ def product_list(request, category_slug=None):
                   'shop/product/list.html',
                   {'category': category,
                    'categories': categories, 
+                   'products': products})"""
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
                    'products': products})
+
 
 
 def product_detail(request, id, slug):
