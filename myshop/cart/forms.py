1a4b6e9159c0f434a380daa1087ce61f960436ff
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-05-01 05:47:31 +0300

    add product_detail styles

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -1,8 +1,9 @@
 from django import forms
-
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Div, Submit
+from crispy_forms.bootstrap import PrependedText, PrependedAppendedText
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
-
 
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
@@ -11,3 +12,20 @@ class CartAddProductForm(forms.Form):
     override = forms.BooleanField(required=False,
                                   initial=False,
                                   widget=forms.HiddenInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Row(
+                Div(
+                    PrependedAppendedText('quantity',
+                                          PrependedText('<button class="btn btn-outline-primary js-btn-minus" type="button">&minus;</button>',
+                                                        '<button class="btn btn-outline-primary js-btn-plus" type="button">&plus;</button>'),
+                                          css_class='input-group mb-3')
+                ),
+                css_class='col-auto'
+            ),
+            Submit('submit', 'Купить парфюмерную продукцию', css_class='btn btn-dark btn-block')
+        )
